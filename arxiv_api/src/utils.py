abb42c5960c329f9487c0fcb7c874f949534e2a5
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2026-02-19 19:11:08 +0000

    add utils

---
+++ arxiv_api/src/utils.py
@@ -0,0 +1,15 @@
+from arxiv_lib.db_models.models import Paper
+
+
+def paper_to_dict(paper: Paper) -> dict:
+    return {
+        "id": str(paper.id),
+        "arxiv_id": paper.arxiv_id,
+        "title": paper.title,
+        "published_date": paper.published_date.isoformat() if paper.published_date else None,
+        "pdf_processed": paper.pdf_processed,
+        "pdf_processing_date": (
+            paper.pdf_processing_date.isoformat()
+            if paper.pdf_processing_date else None
+        )
+    }
