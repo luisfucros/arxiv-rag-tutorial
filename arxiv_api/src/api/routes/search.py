d50615e53c3ab6a3a8f7dfd63b5527fc8eab46d6
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2026-02-20 23:58:21 +0000

    add user_id

--- arxiv_api/src/api/routes/search.py
+++ arxiv_api/src/api/routes/search.py
@@ -2,20 +2,22 @@ from api.dependencies import get_search_engine_service
 from fastapi import APIRouter, Depends
 from schemas.search import SearchRequest, SearchResponse
 from services.search import SearchEngineService
-from api.dependencies import get_current_user
+from api.dependencies import CurrentUser
 
 
-router = APIRouter(prefix="/search", tags=["Search"], dependencies=[Depends(get_current_user)])
+router = APIRouter(prefix="/search", tags=["Search"])
 
 
 @router.post("/", response_model=SearchResponse)
 def search_papers(
     payload: SearchRequest,
-    search_engine_service: SearchEngineService = Depends(get_search_engine_service),
+    current_user: CurrentUser,
+    search_engine_service: SearchEngineService = Depends(get_search_engine_service)
 ):
     results = search_engine_service.search(
         query=payload.query,
         top_k=payload.top_k,
         filters=payload.filters,
+        user_id=current_user.id
     )
     return SearchResponse(results=results)
