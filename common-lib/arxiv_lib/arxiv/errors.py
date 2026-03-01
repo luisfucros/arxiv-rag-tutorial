7f72b0170a7fd8794bb4fc6b7b55c5d05ca5d2d4
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2026-03-01 22:26:28 +0000

    add logging

--- common-lib/arxiv_lib/arxiv/errors.py
+++ common-lib/arxiv_lib/arxiv/errors.py
@@ -1,9 +1,12 @@
 import functools
+import logging
 
 from arxiv import HTTPError
 from requests.exceptions import ConnectionError
 
 from ..exceptions import FileError, ServiceNotAvailable
+
+logger = logging.getLogger(__name__)
 
 
 def handle_errors(func):
@@ -15,13 +18,45 @@ def handle_errors(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except HTTPError:
+        except HTTPError as e:
+            logger.error(
+                "HTTPError during '%s' [%s.%s]: %s",
+                func.__name__,
+                type(e).__module__,
+                type(e).__qualname__,
+                str(e),
+                exc_info=True,
+            )
             raise
-        except (ConnectionError, TimeoutError):
+        except (ConnectionError, TimeoutError) as e:
+            logger.error(
+                "Arxiv service error during '%s' [%s.%s]: %s",
+                func.__name__,
+                type(e).__module__,
+                type(e).__qualname__,
+                str(e),
+                exc_info=True,
+            )
             raise ServiceNotAvailable("Arxiv service not available")
-        except (FileNotFoundError, IOError, OSError):
+        except (FileNotFoundError, IOError, OSError) as e:
+            logger.error(
+                "File error during '%s' [%s.%s]: %s",
+                func.__name__,
+                type(e).__module__,
+                type(e).__qualname__,
+                str(e),
+                exc_info=True,
+            )
             raise FileError("File error occurred while handling pdf")
-        except Exception:
+        except Exception as e:
+            logger.error(
+                "Arxiv error during '%s' [%s.%s]: %s",
+                func.__name__,
+                type(e).__module__,
+                type(e).__qualname__,
+                str(e),
+                exc_info=True,
+            )
             raise
 
     return wrapper  # type: ignore[return-value]
